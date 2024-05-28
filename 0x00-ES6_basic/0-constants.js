d9bf8d99e1d0117a977a1d4e0d1c6206896d95df
Gerson Antonio <gelsonantonio53@outlook.com>
2024-05-28 07:33:31 +0200

    made corrections to file

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,27 +1,12 @@
-/**
-  * function taskFirst to instantiate variables using const
-  *
-  * @returns {string} The desired string output.
-  */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-/**
-  * function getLast to return a string
-  *
-  * @returns {string} The desired string output.
-  */
 export function getLast() {
   return ' is okay';
 }
 
-/**
-  * function taskNext to combine strings
-  *
-  * @returns {string} The combined string output.
-  */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
