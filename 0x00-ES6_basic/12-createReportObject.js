41d926a04f9e07c7304f1d0250b8e8dcb61dba85
Gerson Antonio <gelsonantonio53@outlook.com>
2024-05-29 02:11:38 +0200

    completeed tasks 7-14

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,16 @@
+export function createEmployeesObject(departmentName, employees) {
+    return { [departmentName]: [...employees] };
+  }
+  
+  // New function to create the report object
+  export default function createReportObject(employeesList) {
+    const allEmployees = { ...employeesList };
+  
+    return {
+      allEmployees,
+      getNumberOfDepartments() {
+        return Object.keys(this.allEmployees).length;
+      },
+    };
+  }
+  
