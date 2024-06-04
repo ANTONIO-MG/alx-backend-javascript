08ca1d3581f900356b2f26e65ffb65759dfbebcf
Gerson Antonio <gelsonantonio53@outlook.com>
2024-06-05 00:28:12 +0200

    completed the readme and task 1

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,23 @@
+export default function getListStudents() {
+  const person1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const person2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const person3 = {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  const array1 = [person1, person2, person3];
+
+  return array1;
+}
