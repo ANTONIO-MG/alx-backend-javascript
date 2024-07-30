8079a07ba6e2fec3882478aef08eb76bc481c1ef
Gerson Antonio <gelsonantonio53@outlook.com>
2024-07-30 23:51:28 +0200

    added the \r to create new line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,14 @@
 process.stdin.setEncoding('utf8');
 
-function great() {
-  console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?');
 
-  process.stdin.on('data', (chunk) => {
-    const name = chunk.toString().trim();
-    console.log(`Your name is: ${name}`);
+process.stdin.on('data', (chunk) => {
+  const name = chunk.toString().trim();
+  console.log(`Your name is: ${name}`);
 
-    if (!process.stdin.isTTY) {
-      console.log('This important software is now closing');
-    }
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
 
-    process.exit();
-  });
-}
-
-great();
+  process.exit();
+});
