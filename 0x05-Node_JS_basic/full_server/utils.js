2e81940c381e1bea43f85186d86b856e474fb8b4
Gerson Antonio <gelsonantonio53@outlook.com>
2024-07-30 22:57:01 +0200

    lints correction

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,31 +1,30 @@
 import fs from 'fs';
 
-const readDatabase = (filePath) =>
-  new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length === 0) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const fields = {};
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
       }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-
-      if (lines.length === 0) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const fields = {};
-      lines.slice(1).forEach((line) => {
-        const [firstname, , , field] = line.split(',');
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      });
-
-      resolve(fields);
+      fields[field].push(firstname);
     });
+
+    resolve(fields);
   });
+});
 
 export default readDatabase;
